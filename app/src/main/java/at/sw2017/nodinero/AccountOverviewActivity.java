bcc06ee016d5631c588ec8dc0f13418b9406a326
Christian Paier <c.paier@student.tugraz.at>
2017-04-11 11:45:25 +0200

    [KM,CP] initial commit

---
+++ app/src/main/java/at/sw2017/nodinero/AccountOverviewActivity.java
@@ -0,0 +1,13 @@
+package at.sw2017.nodinero;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_overview);
+    }
+}
