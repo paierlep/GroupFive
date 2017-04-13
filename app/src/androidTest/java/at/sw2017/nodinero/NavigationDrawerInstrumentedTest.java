943afa6d8556c818469f2f87fe9399063e5553cb
Karin Maier <karin.maier@student.tugraz.at>
2017-04-14 00:45:12 +0200

    [KM,CP] added test for menu item new account

--- app/src/androidTest/java/at/sw2017/nodinero/NavigationDrawerInstrumentedTest.java
+++ app/src/androidTest/java/at/sw2017/nodinero/NavigationDrawerInstrumentedTest.java
@@ -11,10 +11,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.DrawerActions.open;
 import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -34,5 +36,10 @@ public class NavigationDrawerInstrumentedTest {
         onView(withId(R.id.drawer_layout))
                 .check(matches(isClosed(Gravity.LEFT)))
                 .perform(open());
+    }
+
+    @Test
+    public void checkIfAddAccountExists() {
+        onView(withText("Add Account")).perform(click());
     }
 }
