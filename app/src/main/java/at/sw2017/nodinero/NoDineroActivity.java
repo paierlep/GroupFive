3e915f911ada721877378bd08847fbf1a7f8d9c9
Christian Paier <c.paier@student.tugraz.at>
2017-04-14 01:31:34 +0200

    [CP,KM] Adding submenu entry for accounts

--- app/src/main/java/at/sw2017/nodinero/NoDineroActivity.java
+++ app/src/main/java/at/sw2017/nodinero/NoDineroActivity.java
@@ -1,14 +1,21 @@
 package at.sw2017.nodinero;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
 
 import at.sw2017.nodinero.model.Database;
 
-public class NoDineroActivity extends AppCompatActivity {
+public class NoDineroActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    public final String TAG = "NoDineroActivity";
+
+    private NavigationView navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,15 @@ public class NoDineroActivity extends AppCompatActivity {
 
         FlowManager.init(new FlowConfig.Builder(this).build());
         FlowManager.getDatabase(Database.class).getWritableDatabase();
+
+        navigation = (NavigationView) findViewById(R.id.nav_view);
+
+        navigation.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Log.d(TAG, "pushed menu button " + item.getTitle());
+        return true;
     }
 }
