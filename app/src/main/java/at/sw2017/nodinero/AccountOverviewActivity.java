c27af79a8e2bb9baa95b0e80716b2c57d440f698
michael ko <koshak@gmx.at>
2017-04-15 10:05:20 +0200

    [MK, MT] fragments and API 21
    created overview fragment and profile fragment. changed to API 21 for
    material design "standards"

--- app/src/main/java/at/sw2017/nodinero/AccountOverviewActivity.java
+++ app/src/main/java/at/sw2017/nodinero/AccountOverviewActivity.java
@@ -1,7 +1,17 @@
 package at.sw2017.nodinero;
 
+import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import fragments.OverviewFragment;
+import fragments.SettingsFragment;
 
 public class AccountOverviewActivity extends AppCompatActivity {
 
@@ -9,5 +19,43 @@ public class AccountOverviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_overview);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, new OverviewFragment()).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.app_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment fragment;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.fragment_container, new SettingsFragment()).commit();
+                return true;
+
+            case R.id.action_profile:
+                Toast toast = Toast.makeText(getApplicationContext(), "profile dummy", Toast.LENGTH_SHORT);
+                toast.show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
---
+++ app/src/main/java/fragments/SettingsFragment.java
@@ -0,0 +1,33 @@
+package fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import at.sw2017.nodinero.R;
+
+/**
+ * Created by kosha on 14/04/2017.
+ */
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
