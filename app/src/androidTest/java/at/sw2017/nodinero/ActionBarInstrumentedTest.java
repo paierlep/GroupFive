2af7effe70ff26bcfd8152c87053afed503bd40e
Karin Maier <karin.maier@student.tugraz.at>
2017-04-18 20:59:35 +0200

    [KM,CP] created simple ActionBar

--- app/src/androidTest/java/at/sw2017/nodinero/ActionBarInstrumentedTest.java
+++ app/src/androidTest/java/at/sw2017/nodinero/ActionBarInstrumentedTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Instrumentation test, which will execute on an Android device.
