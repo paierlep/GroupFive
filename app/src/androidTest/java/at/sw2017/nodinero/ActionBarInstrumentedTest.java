cbf9c176b7a603744b0067d3ea3695b9e93dcb20
Christian Paier <c.paier@student.tugraz.at>
2017-04-18 20:16:29 +0200

    [CP,KM] action bar test file

---
+++ app/src/androidTest/java/at/sw2017/nodinero/ActionBarInstrumentedTest.java
@@ -0,0 +1,31 @@
+package at.sw2017.nodinero;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.Gravity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ActionBarInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<NoDineroActivity> mActivityTestRule =
+            new ActivityTestRule<>(NoDineroActivity.class);
+
+    @Test
+    public void checkToolBarExists() {
+        onView(withId(R.id.menu_bar)).check(matches(isDisplayed()));
+    }
+}
