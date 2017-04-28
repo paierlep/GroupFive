89b12057a7fc81463d0431a10b8e9ca0eabb9842
Harald Deutschmann <harald.deutschmann@student.tugraz.at>
2017-04-28 16:49:02 +0200

    [JH, HD] implemented account swipe delete

--- app/src/main/java/at/sw2017/nodinero/fragment/AccountOverviewFragment.java
+++ app/src/main/java/at/sw2017/nodinero/fragment/AccountOverviewFragment.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintSet;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.widget.AppCompatSpinner;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Display;
+import android.view.GestureDetector;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateInterpolator;
@@ -51,6 +54,8 @@ public class AccountOverviewFragment extends Fragment implements View.OnClickLis
     public final String TAG = "AccountOverviewFragment";
     private View account_view;
     private int count_accounts = 0;
+    private GestureDetectorCompat gestureDetector;
+    private View current_view = null;
     public static AccountOverviewFragment newInstance() {
         Bundle args = new Bundle();
         AccountOverviewFragment fragment = new AccountOverviewFragment();
@@ -62,6 +67,7 @@ public class AccountOverviewFragment extends Fragment implements View.OnClickLis
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         account_view=inflater.inflate(R.layout.fragment_account_overview, container, false);
+        gestureDetector = new GestureDetectorCompat(this.getContext(), new GestureListener());
         createAccount();
         return account_view;
     }
@@ -72,6 +78,7 @@ public class AccountOverviewFragment extends Fragment implements View.OnClickLis
             if(v.getId() == accounts.name.hashCode())
             {
                 Toast.makeText(this.getActivity(), "not implemented yet!", Toast.LENGTH_LONG).show();
+
             }
             else if(v.getId() ==  accounts.id)
             {
@@ -80,16 +87,29 @@ public class AccountOverviewFragment extends Fragment implements View.OnClickLis
                         .async()
                         .execute();
 
+              //  v.animate().translationX(v.getWidth());
+                final TableRow row = (TableRow) v.getParent();
+                final TableLayout table = (TableLayout) row.getParent();
+                final Animation fadeout = AnimationUtils.loadAnimation(this.getContext(), android.R.anim.fade_out);
+                fadeout.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                    }
 
-                TableRow row = (TableRow) v.getParent();
-                TableLayout table = (TableLayout) row.getParent();
-                Animation fadeout = AnimationUtils.loadAnimation(this.getContext(), android.R.anim.fade_out);
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        table.removeView(row);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
                 fadeout.setDuration(1000);
                 row.startAnimation(fadeout);
                 row.setVisibility(View.INVISIBLE);
 
-
-                table.removeView(row);
             }
         }
 
@@ -115,10 +135,13 @@ public class AccountOverviewFragment extends Fragment implements View.OnClickLis
             TableRow append_row = new TableRow(this.getContext());
             append_row.setGravity(Gravity.CENTER);
 
-            Button myButton = new Button(this.getContext());
-            myButton.setId(accounts.id);
-            myButton.setText(R.string.account_delete);
-            myButton.setOnClickListener(this);
+            final Button account_delete_button = new Button(this.getContext());
+            account_delete_button.setId(accounts.id);
+            account_delete_button.setText(R.string.account_delete);
+            account_delete_button.setOnClickListener(this);
+            account_delete_button.setVisibility(View.INVISIBLE);
+            //account_delete_button.setX((float)(0.5*width));
+
             TableRow.LayoutParams params_view = new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT, 1.0f);
 
 
@@ -154,15 +177,55 @@ public class AccountOverviewFragment extends Fragment implements View.OnClickLis
             append_row.addView(view_name);
             append_row.addView(view_balance);
             append_row.addView(view_currency);
-            append_row.addView(myButton);
+            append_row.addView(account_delete_button);
             append_row.setBackgroundResource(R.drawable.account_row_border);
             append_row.setId(accounts.name.hashCode());
             append_row.setClickable(true);
             append_row.setOnClickListener(this);
 
+            append_row.setOnTouchListener(new View.OnTouchListener(){
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                gestureDetector.onTouchEvent(event);
+                current_view = account_delete_button;
+                return true;
+            }});
+
             account_table.addView(append_row);
         }
 
+    }
+
+    class GestureListener extends GestureDetector.SimpleOnGestureListener { // change a little bit
+        private static final String TAG = "Gestures";
+
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MAX_OFF_PATH = 250;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 100;
+
+        @Override
+        public boolean onFling(MotionEvent swipe1, MotionEvent swipe2,
+                               float velocityX, float velocityY) {
+            
+            try {
+                if (Math.abs(swipe1.getY() - swipe2.getY()) > SWIPE_MAX_OFF_PATH)
+                    return false;
+                if (swipe1.getX() - swipe2.getX() > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    Log.d(TAG, "Swipe to the left" + current_view.getId());
+                    current_view.setVisibility(View.VISIBLE);
+                } else if (swipe2.getX() - swipe1.getX() > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    current_view.setVisibility(View.INVISIBLE);
+                    Log.d(TAG, "Swipe to the right" + current_view.getId());
+                }
+            } catch (Exception e) {
+                return false;
+            }
+            return true;
+        }
     }
 
     public String getTAG() {
