b82e8ab512b3ef2ead46e1ab42ef920165729b7c
Karin Maier <karin.maier@student.tugraz.at>
2017-05-30 23:21:26 +0200

    [JH,KM] created button for photo in expense

--- app/src/androidTest/java/at/sw2017/nodinero/ExpansesPhotoInstrumentedTest.java
+++ app/src/androidTest/java/at/sw2017/nodinero/ExpansesPhotoInstrumentedTest.java
@@ -1,12 +1,8 @@
 package at.sw2017.nodinero;
 
-import android.app.Activity;
-import android.app.Instrumentation;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.Gravity;
+import android.view.ViewGroup;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,9 +11,13 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerActions.open;
+import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -25,14 +25,44 @@ public class ExpansesPhotoInstrumentedTest extends AbstractNoDineroInstrumentedT
 
 
     @Test
-    public void takePictureButtonExists() {
-        onView(withId(R.id.button_image)).perform(scrollTo()).check(matches(isDisplayed())).perform(click());
+    public void addExpense()
+    {
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.START)))
+                .perform(open());
+
+        onView(withText(R.string.add_expense)).perform(click());
+        onView(withId(R.id.fragment_expense_add)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.expense_account_type_spinner)).check(matches(isDisplayed()));
+        onView(withId(R.id.expense_category_spinner)).check(matches(isDisplayed()));
+        onView(withId(R.id.expense_date_picker)).check(matches(isDisplayed()));
+        onView(withId(R.id.expense_name)).check(matches(isDisplayed()));
+        onView(withId(R.id.expense_value)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.button_image)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.button_save)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_cancel)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_save_back)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.expense_name)).check(matches(isDisplayed()));
+
+        String expense_test = "Beer";
+
+        onView(withId(R.id.expense_name)).perform(typeText(expense_test));
+
+        onView(withId(R.id.button_save)).perform(scrollTo()).check(matches(isDisplayed())).perform(click());
+        onView(withId(R.id.fragment_expense_add)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.START)))
+                .perform(open());
+        onView(withText(R.string.account_overview)).perform(click());
+
+        onView(withId(R.id.fragment_account_overview)).check(matches(isDisplayed()));
+
+
+
     }
-
-    @Test
-    public void validateCameraScenario() {
-
-    }
-
-
 }
--- app/src/main/java/at/sw2017/nodinero/fragment/ExpenseFormFragment.java
+++ app/src/main/java/at/sw2017/nodinero/fragment/ExpenseFormFragment.java
@@ -1,20 +1,34 @@
 package at.sw2017.nodinero.fragment;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatSpinner;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 
 import com.raizlabs.android.dbflow.sql.language.SQLite;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import at.sw2017.nodinero.NoDineroActivity;
@@ -22,7 +36,6 @@ import at.sw2017.nodinero.R;
 import at.sw2017.nodinero.adapter.AccountAdapter;
 import at.sw2017.nodinero.adapter.CategoryAdapter;
 import at.sw2017.nodinero.model.Account;
-import at.sw2017.nodinero.model.Account_Table;
 import at.sw2017.nodinero.model.Category;
 import at.sw2017.nodinero.model.Expense;
 import at.sw2017.nodinero.model.Expense_Table;
@@ -33,11 +46,15 @@ import at.sw2017.nodinero.model.Expense_Table;
 
 public class ExpenseFormFragment extends Fragment implements View.OnClickListener{
     final private String TAG = "AddExpenseFragement";
+    private static final int SELECT_PICTURE = 1;
+    private static final int TAKE_PHOTO     = 0;
 
     private AppCompatButton saveButton;
     private AppCompatButton editButton;
     private AppCompatButton saveAndBackButton;
     private AppCompatButton cancelButton;
+    private AppCompatButton takePictureButton;
+    private ImageView imageView;
 
     private TextInputEditText expenseName;
     private TextInputEditText expenseValue;
@@ -46,6 +63,9 @@ public class ExpenseFormFragment extends Fragment implements View.OnClickListene
     private AppCompatSpinner expenseAccount;
     private int currentAccountId;
     private int currentCategoryId;
+    private String currentPhotoPath;
+    private ImageView userImagePreview;
+    private Uri currentPhotoFile;
 
     private Expense expense;
 
@@ -93,6 +113,12 @@ public class ExpenseFormFragment extends Fragment implements View.OnClickListene
         expenseCategory.setAdapter(categoryAdapter);
         expenseCategory.setSelection(categoryAdapter.getPos(currentCategoryId));
 
+        takePictureButton = (AppCompatButton) view.findViewById(R.id.button_image);
+        imageView = (ImageView) view.findViewById(R.id.imageview);
+      //  if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.CAMERA)) {
+      //      takePictureButton.setEnabled(false);
+      //      ActivityCompat.requestPermissions(this.getActivity(), new String[] {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+      //  }
 
         saveButton = (AppCompatButton) view.findViewById(R.id.button_save);
         saveButton.setOnClickListener(this);
@@ -135,10 +161,6 @@ public class ExpenseFormFragment extends Fragment implements View.OnClickListene
         accountAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         expenseAccount.setAdapter(accountAdapter);
         expenseAccount.setSelection(accountAdapter.getPos(currentAccountId));
-
-
-
-
 
         return view;
     }
@@ -209,8 +231,84 @@ public class ExpenseFormFragment extends Fragment implements View.OnClickListene
         } else if (v.getId() == R.id.button_edit) {
             editExpense();
             loadExpanseCorrectView();
+        } else if (v.getId() == R.id.button_image) {
+            takePhoto();
+        }
+        
+    }
+
+    private void takePhoto() {
+        Log.e("PERMS", "take photo");
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(this.getContext().getPackageManager()) != null) {
+            Log.e("PERMS", "create File");
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Log.e("TAG", "exception" + ex);
+            }
+
+            if (photoFile != null) {
+                Log.e(TAG, "calling photo app");
+                currentPhotoFile = Uri.fromFile(photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        currentPhotoFile);
+                startActivityForResult(takePictureIntent, 0);
+            }
         }
     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == this.getActivity().RESULT_OK) {
+            if (requestCode == TAKE_PHOTO) {
+                //Bundle extras = data.getExtras();
+                //Bitmap imageBitmap = (Bitmap) extras.get("data");
+                Log.e("HOT", "return from camera");
+                Bitmap bitmap;
+                try {/*
+                    Image image = new Image(currentPhotoFile, getActivity().getContentResolver());
+                    userImagePreview.setImageBitmap(image.getBitmap());
+                    ByteArrayOutputStream bao = new ByteArrayOutputStream();
+                    bitmap = image.getBitmap();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 90, bao);
+                    byte[] ba = bao.toByteArray();
+                    String byteCode = Base64.encodeToString(ba, Base64.DEFAULT);
+                    new ChildImageUploadRequest(
+                            Utils.getDeviceId(context),
+                            currentUser.getId(),
+                            currentPerson.id,
+                            byteCode).
+                            send(handler, (ActivityHandler) getActivity());
+                    // TODO */
+                } catch (Exception e) {
+                    Log.e("TAG", "exception " + e);
+                    //TODO Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+                }
+            } else if (requestCode == SELECT_PICTURE) {
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
 
     private void loadExpanseCorrectView(){
         if (currentAccountId > 0) {
